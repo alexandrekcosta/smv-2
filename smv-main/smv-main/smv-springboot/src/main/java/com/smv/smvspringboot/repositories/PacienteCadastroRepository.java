0bd15e35f13c5fd1dcd2f3889524b3385db25dcb
Alexandre Costa <68577176+alexandrekcosta@users.noreply.github.com>
2022-11-09 09:06:55 -0300

    Add files via upload

---
+++ smv-main/smv-main/smv-springboot/src/main/java/com/smv/smvspringboot/repositories/PacienteCadastroRepository.java
@@ -0,0 +1,8 @@
+package com.smv.smvspringboot.repositories;
+
+import entidade.PacienteCadastroEntity;
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+public interface PacienteCadastroRepository extends JpaRepository<PacienteCadastroEntity> {
+
+}
